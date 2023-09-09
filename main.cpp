319eab9ea916ed03aae27c77f0382a180cff59a7
Deep2000Dev <126567314+Deep2000Dev@users.noreply.github.com>
2023-09-09 23:33:48 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,34 +1,31 @@
-#include <iostream>
-#include <ctime>
-
-int main() {
-    // Seed the random number generator with the current time
-    unsigned int seed = static_cast<unsigned int>(time(NULL));
-    srand(seed);
-
-    // Generate a random number between 1 and 100
-    int secretNumber = rand() % 100 + 1;
-    int userGuess;
-    int attempts = 0;
-
-    std::cout << "Welcome to the Guess the Number game!" << std::endl;
-    std::cout << "I have selected a random number between 1 and 100." << std::endl;
-
-    while (true) {
-        std::cout << "Guess the number: ";
-        std::cin >> userGuess;
-        attempts++;
-
-        if (userGuess == secretNumber) {
-            std::cout << "Congratulations! You guessed the correct number " << secretNumber << " in " << attempts << " attempts." << std::endl;
-            break;
-        } else if (userGuess < secretNumber) {
-            std::cout << "Too low. Try again." << std::endl;
-        } else {
-            std::cout << "Too high. Try again." << std::endl;
-        }
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter a number"<<endl;
+    cin>>a;
+    int b;
+    cout<<" enter another number"<<endl;
+    cin>>b;
+    char op;
+    cout<<"enter the operator"<<endl;
+    cin>>op;
+    if(op=='*')
+    {
+        cout<<a*b<<endl;
     }
-   
-
+    else if(op=='/')
+    {
+        cout<< a/b<<endl;
+    }
+    else if(op=='+')
+    {
+        cout<<a+b<<endl;
+    }
+    else if(op=='-')
+    {
+        cout<<a-b<<endl;
+    }
     return 0;
 }
