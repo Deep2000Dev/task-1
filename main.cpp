1c051c1e608a34c1868284a6f750f48b42747df8
Deep2000Dev <126567314+Deep2000Dev@users.noreply.github.com>
2023-09-09 23:38:54 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,31 +1,98 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    int a;
-    cout<<"enter a number"<<endl;
-    cin>>a;
-    int b;
-    cout<<" enter another number"<<endl;
-    cin>>b;
-    char op;
-    cout<<"enter the operator"<<endl;
-    cin>>op;
-    if(op=='*')
-    {
-        cout<<a*b<<endl;
+
+// Function to display the Tic-Tac-Toe board
+void displayBoard(char board[3][3]) {
+    cout << "TIC-TAC-TOE GAME" << endl;
+    for (int i = 0; i < 3; i++) {
+        cout << "-------------" << endl;
+        for (int j = 0; j < 3; j++) {
+            cout << "| " << board[i][j] << " ";
+        }
+        cout << "|" << endl;
     }
-    else if(op=='/')
-    {
-        cout<< a/b<<endl;
+    cout << "-------------" << endl;
+}
+
+// Function to check if a player has won
+bool checkWin(char board[3][3], char player) {
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
+            return true;
+        }
+        if (board[0][i] == player && board[1][i] == player && board[2][i] == player) {
+            return true;
+        }
     }
-    else if(op=='+')
-    {
-        cout<<a+b<<endl;
+    if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+        return true;
     }
-    else if(op=='-')
-    {
-        cout<<a-b<<endl;
+    if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
+        return true;
     }
+    return false;
+}
+
+// Function to check if the game is a draw
+bool checkDraw(char board[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i][j] != 'X' && board[i][j] != 'O') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main() {
+    char board[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+    char currentPlayer = 'X';
+    bool gameOver = false;
+
+    while (!gameOver) {
+        displayBoard(board);
+        cout << "Player " << currentPlayer << ", enter your move (row and column): ";
+        int row, col;
+        cin >> row >> col;
+
+        if (row < 0 || row >= 3 || col < 0 || col >= 3 || board[row][col] != ' ') {
+            cout << "Invalid move. Try again." << endl;
+            continue;
+        }
+
+        board[row][col] = currentPlayer;
+
+        if (checkWin(board, currentPlayer)) {
+            displayBoard(board);
+            cout << "Player " << currentPlayer << " wins!" << endl;
+            gameOver = true;
+        } else if (checkDraw(board)) {
+            displayBoard(board);
+            cout << "It's a draw!" << endl;
+            gameOver = true;
+        }
+
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+
+    char playAgain;
+    cout << "Play again? (Y/N): ";
+    cin >> playAgain;
+
+    if (playAgain == 'Y' || playAgain == 'y') {
+        // Reset the game and play again
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = ' ';
+            }
+        }
+        gameOver = false;
+        currentPlayer = 'X';
+    } else {
+        cout << "Thanks for playing! Goodbye!" << endl;
+    }
+
     return 0;
 }
+
